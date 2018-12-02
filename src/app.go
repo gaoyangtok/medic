e41fe85b2fbf3ea35fae181d7728dafd7ff56a7a
unknown <hanfengd@digitalchina.com>
2018-12-02 23:00:17 +0800

    新增更新

--- src/app.go
+++ src/app.go
@@ -6,8 +6,9 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
+	. "medic/src/data"
 	"sort"
+	"strconv"
 	"strings"
 	"syscall"
 	"time"
@@ -18,53 +19,93 @@ import (
 	. "github.com/lxn/walk/declarative"
 )
 
-type Foo struct {
-	Index		int
-	Name		string
-	Phone		float64
-	Create		time.Time
-	Update		time.Time
-	Addr			string
-	Message	string
-	checked bool
-}
-
 type FooModel struct {
 	walk.TableModelBase
 	walk.SorterBase
-	sortColumn int
-	sortOrder  walk.SortOrder
-	items      []*Foo
+	sortColumn	int
+	sortOrder	walk.SortOrder
+	search		*Search
+	items		[]*Foo
+	sItems		[]*Foo
+	sum 		float64
+	sSum		float64
+	lSum		float64
+	SumLabel	*walk.Label
+	SSumLabel	*walk.Label
+	LSumLabel	*walk.Label
 }
 
 func NewFooModel() *FooModel {
 	m := new(FooModel)
+	m.sortColumn=3
+	m.sortOrder = 0
+	m.sItems = Read()
+	m.items = m.sItems[0:]
+	copy(m.items,m.sItems)
+	m.search = new(Search)
+	m.SumLabel = new(walk.Label)
+	m.SSumLabel = new(walk.Label)
+	m.LSumLabel = new(walk.Label)
+	for _,item := range m.items{
+		m.sum=m.sum+item.PaidFee
+		if item.Create.Month() == time.Now().Month(){
+			m.sSum = m.sSum+item.PaidFee
+		}
+		if item.RealFee>item.PaidFee{
+			m.lSum = m.lSum+item.RealFee-item.PaidFee
+		}
+	}
+	m.search.Start, _ = time.Parse("2006-01-02 15:04:05","2018-12-01 00:00:00")
+	m.search.End= time.Now().Add(time.Hour*24)
 	m.ResetRows()
 	return m
+}
+
+func (m *FooModel) GetSearch() *Search {
+	return m.search
 }
 
 // Called by the TableView from SetModel and every time the model publishes a
 // RowsReset event.
 func (m *FooModel) RowCount() int {
-	return len(m.items)
+	return len(m.sItems)
+}
+
+func (m *FooModel) Head(foo *Foo) {
+	m.items=append(append([]*Foo{},foo), m.items...)
+	m.sItems=append(append([]*Foo{},foo), m.sItems...)
 }
 
 // Called by the TableView when it needs the text to display for a given cell.
 func (m *FooModel) Value(row, col int) interface{} {
-	item := m.items[row]
-
+	item := m.sItems[row]
 	switch col {
 	case 0:
-		return item.Index
-
+		return ""
 	case 1:
-		return item.Bar
-
+		return item.Name
 	case 2:
-		return item.Baz
-
+		return item.Phone
 	case 3:
-		return item.Quux
+		return item.Sex
+	case 4:
+		return item.Age
+	case 5:
+		return item.AllFee
+	case 6:
+		return item.RealFee
+	case 7:
+		return item.PaidFee
+	case 8:
+		return item.Create
+	case 9:
+		return item.Update
+	case 10:
+		return item.Diagnosed
+	case 11:
+		return item.Program
+	case 12:
+		return item.Address
 	}
 
 	panic("unexpected col")
@@ -72,13 +113,12 @@ func (m *FooModel) Value(row, col int) interface{} {
 
 // Called by the TableView to retrieve if a given row is checked.
 func (m *FooModel) Checked(row int) bool {
-	return m.items[row].checked
+	return m.sItems[row].Checked
 }
 
 // Called by the TableView when the user toggled the check box of a given row.
 func (m *FooModel) SetChecked(row int, checked bool) error {
-	m.items[row].checked = checked
-
+	m.sItems[row].Checked = checked
 	return nil
 }
 
@@ -86,31 +126,44 @@ func (m *FooModel) SetChecked(row int, checked bool) error {
 func (m *FooModel) Sort(col int, order walk.SortOrder) error {
 	m.sortColumn, m.sortOrder = col, order
 
-	sort.SliceStable(m.items, func(i, j int) bool {
-		a, b := m.items[i], m.items[j]
+	sort.SliceStable(m.sItems, func(i, j int) bool {
+		a, b := m.sItems[i], m.sItems[j]
 
 		c := func(ls bool) bool {
 			if m.sortOrder == walk.SortAscending {
 				return ls
 			}
-
 			return !ls
 		}
 
 		switch m.sortColumn {
 		case 0:
 			return c(a.Index < b.Index)
-
 		case 1:
-			return c(a.Bar < b.Bar)
-
+			return c(a.Name < b.Name)
 		case 2:
-			return c(a.Baz < b.Baz)
-
+			return c(a.Phone < b.Phone)
 		case 3:
-			return c(a.Quux.Before(b.Quux))
+			return c(a.Sex < b.Sex)
+		case 4:
+			return c(a.Age < b.Age)
+		case 5:
+			return c(a.AllFee < b.AllFee)
+		case 6:
+			return c(a.RealFee < b.RealFee)
+		case 7:
+			return c(a.PaidFee < b.PaidFee)
+		case 8:
+			return c(a.Create.Before(b.Create))
+		case 9:
+			return c(a.Update.Before(b.Update))
+		case 10:
+			return c(a.Diagnosed < b.Diagnosed)
+		case 11:
+			return c(a.Program < b.Program)
+		case 12:
+			return c(a.Address < b.Address)
 		}
-
 		panic("unreachable")
 	})
 
@@ -118,24 +171,23 @@ func (m *FooModel) Sort(col int, order walk.SortOrder) error {
 }
 
 func (m *FooModel) ResetRows() {
-	// Create some random data.
-	m.items = make([]*Foo, rand.Intn(50000))
-
-	now := time.Now()
-
-	for i := range m.items {
-		m.items[i] = &Foo{
-			Index: i,
-			Bar:   strings.Repeat("*", rand.Intn(5)+1),
-			Baz:   rand.Float64() * 1000,
-			Quux:  time.Unix(rand.Int63n(now.Unix()), 0),
-		}
-	}
-
 	// Notify TableView and other interested parties about the reset.
 	m.PublishRowsReset()
+	_ = m.Sort(m.sortColumn, m.sortOrder)
+}
 
-	m.Sort(m.sortColumn, m.sortOrder)
+func (m *FooModel) Search() {
+	sItems := []*Foo{}
+	start:=m.search.Start.Format("2006-01-02 15:04:05")
+	end:=m.search.End.Format("2006-01-02 15:04:05")
+	for _,item:=range m.items{
+		create:=item.Create.Format("2006-01-02 15:04:05")
+		if strings.Contains(item.Name,m.search.Name) && create>start && create<end && strings.Contains(item.Phone,m.search.Phone) {
+			sItems = append(sItems,item)
+		}
+	}
+	m.sItems = sItems
+	m.ResetRows()
 }
 
 var (
@@ -154,18 +206,33 @@ const (
 	SM_CYSCREEN = 1
 )
 
+var model = NewFooModel()
+
+type Search struct {
+	Name 	string
+	Phone 	string
+	Start 	time.Time
+	End		time.Time
+}
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
+	fmt.Println("hello world!")
+	walk.FocusEffect, _ = walk.NewBorderGlowEffect(walk.RGB(0, 63, 255))
+	walk.InteractionEffect, _ = walk.NewDropShadowEffect(walk.RGB(63, 63, 63))
+	walk.ValidationErrorEffect, _ = walk.NewBorderGlowEffect(walk.RGB(255, 0, 0))
 
-	boldFont, _ := walk.NewFont("Segoe UI", 9, walk.FontBold)
+	with:=GetSystemMetrics(SM_CXSCREEN)
+	height:=GetSystemMetrics(SM_CYSCREEN)
+	//boldFont, _ := walk.NewFont("Segoe UI", 9, walk.FontBold)
+
+	inputWidth:=(with*80/100-30*3-60*2-150)/4
 
 	labelFont:=Font{Family:"Microsoft YaHei UI",PointSize:9}
 
  	goodIcon, _ := walk.Resources.Icon("img/check.ico")
-	badIcon, _ := walk.Resources.Icon("img/stop.ico")
+	//badIcon, _ := walk.Resources.Icon("img/stop.ico")
 
-	barBitmap, err := walk.NewBitmap(walk.Size{100, 1})
+	barBitmap, err := walk.NewBitmap(walk.Size{Width: 100, Height: 1})
 	if err != nil {
 		panic(err)
 	}
@@ -177,95 +244,99 @@ func main() {
 	}
 	defer barBitmap.Dispose()
 
-	canvas.GradientFillRectangle(walk.RGB(255, 0, 0), walk.RGB(0, 255, 0), walk.Horizontal, walk.Rectangle{0, 0, 100, 1})
+	_ = canvas.GradientFillRectangle(walk.RGB(255, 0, 0), walk.RGB(0, 255, 0), walk.Horizontal, walk.Rectangle{0, 0, 100, 1})
 
 	canvas.Dispose()
 
-	model := NewFooModel()
-
 	var tv *walk.TableView
-
-	var queryPB,caculPB *walk.PushButton
-
-	MainWindow{
-		Size:   Size{GetSystemMetrics(SM_CXSCREEN)*80/100, GetSystemMetrics(SM_CYSCREEN)*80/100},
-		Layout: VBox{},
-		Background: SystemColorBrush{walk.SysColorWindow},
-		Icon: goodIcon,
-		Title: "就诊记录",
-		//OnSizeChanged: func() {
-		//	fmt.Println("OnSizeChanged")
-		//},
-		OnMouseUp: func(x, y int, button walk.MouseButton) {
-			fmt.Println("OnMouseUp")
-		},
-		OnMouseDown: func(x, y int, button walk.MouseButton) {
-			fmt.Println("OnMouseDown")
-		},
+	var db *walk.DataBinder
+	var queryPB,addPB *walk.PushButton
+	var mw *walk.MainWindow
+	_, _ = MainWindow{
+		AssignTo:   &mw,
+		Size:       Size{Width: with * 90 / 100, Height: height - 150},
+		Layout:     VBox{},
+		Background: SystemColorBrush{Color: walk.SysColorWindow},
+		Icon:       goodIcon,
+		Title:      "就诊记录",
 		Children: []Widget{
 			Composite{
-				Layout: Grid{Columns:10},
-				Children:[]Widget{
+				DataBinder: DataBinder{
+					AssignTo:       &db,
+					Name:           "search",
+					DataSource:     model.GetSearch(),
+					ErrorPresenter: ToolTipErrorPresenter{},
+				},
+				Layout:  Grid{Columns: 10},
+				MaxSize: Size{Width: with * 80 / 100, Height: 40},
+				MinSize: Size{Width: with * 80 / 100, Height: 40},
+				Children: []Widget{
 					Label{
-						Text: "姓名:",
-						Font: labelFont,
-						MaxSize: Size{Width:30},
-						MinSize: Size{Width:30},
+						Text:    "姓名:",
+						Font:    labelFont,
+						MaxSize: Size{Width: 30},
+						MinSize: Size{Width: 30},
 					},
 					LineEdit{
-						Text: Bind("Name"),
-						MaxSize: Size{Width:200,Height:20},
-						MinSize: Size{Width:100,Height:20},
+						Text:    Bind("Name"),
+						MaxSize: Size{Width: inputWidth, Height: 20},
+						MinSize: Size{Width: inputWidth, Height: 20},
 					},
 					Label{
-						Text: "号码:",
-						Font: labelFont,
-						MaxSize: Size{Width:30},
-						MinSize: Size{Width:30},
+						Text:    "号码:",
+						Font:    labelFont,
+						MaxSize: Size{Width: 30},
+						MinSize: Size{Width: 30},
 					},
 					LineEdit{
-						Text: Bind("Phone"),
-						MaxSize: Size{Width:200,Height:20},
-						MinSize: Size{Width:100,Height:20},
+						Text:    Bind("Phone"),
+						MaxSize: Size{Width: inputWidth, Height: 20},
+						MinSize: Size{Width: inputWidth, Height: 20},
 					},
 
 					Label{
-						Text: "日期:",
-						Font:labelFont,
-						MaxSize: Size{Width:30},
-						MinSize: Size{Width:30},
+						Text:    "日期:",
+						Font:    labelFont,
+						MaxSize: Size{Width: 30},
+						MinSize: Size{Width: 30},
 					},
 					DateEdit{
-						Date: Bind("Start"),
-						MaxSize: Size{Width:200,Height:20},
-						MinSize: Size{Width:100,Height:20},
+						Date:    Bind("Start"),
+						MaxSize: Size{Width: inputWidth, Height: 20},
+						MinSize: Size{Width: inputWidth, Height: 20},
 					},
 					Label{
-						Text: "-",
-						MaxSize: Size{Width:5},
-						MinSize: Size{Width:5},
+						Text:    "-",
+						MaxSize: Size{Width: 5},
+						MinSize: Size{Width: 5},
 					},
 					DateEdit{
-						Date: Bind("End"),
-						MaxSize: Size{Width:200,Height:20},
-						MinSize: Size{Width:100,Height:20},
+						Date:    Bind("End"),
+						MaxSize: Size{Width: inputWidth, Height: 20},
+						MinSize: Size{Width: inputWidth, Height: 20},
 					},
 					PushButton{
 						AssignTo: &queryPB,
 						Text:     "查询",
-						Font:labelFont,
-						MaxSize: Size{Width:60,Height:20},
-						MinSize: Size{Width:60,Height:20},
+						Font:     labelFont,
+						MaxSize:  Size{Width: 60, Height: 20},
+						MinSize:  Size{Width: 60, Height: 20},
 						OnClicked: func() {
+							if err := db.Submit(); err == nil {
+								model.Search()
+							}
 						},
 					},
 					PushButton{
-						AssignTo: &caculPB,
-						Text:     "计算",
-						Font:labelFont,
-						MaxSize: Size{Width:60,Height:20},
-						MinSize: Size{Width:60,Height:20},
+						AssignTo: &addPB,
+						Text:     "登记",
+						Font:     labelFont,
+						MaxSize:  Size{Width: 60, Height: 20},
+						MinSize:  Size{Width: 60, Height: 20},
 						OnClicked: func() {
+							if cmd, err := AddDialog(mw, nil); err == nil && cmd == walk.DlgCmdOK {
+								model.ResetRows()
+							}
 						},
 					},
 				},
@@ -277,62 +348,271 @@ func main() {
 				CheckBoxes:            true,
 				ColumnsOrderable:      true,
 				MultiSelection:        true,
+				//MinSize:Size{Width:with*75/100,Height:height-300},
+				//MaxSize:Size{Width:with,Height:height-100},
 				Columns: []TableViewColumn{
-					{Title: "#"},
-					{Title: "姓名"},
-					{Title: "电话", Alignment: AlignFar},
-					{Format: "2006-01-02 15:04:05", Title: "登记时间"},
-					{Format: "2006-01-02 15:04:05", Title: "最新时间"},
-					{Title: "就诊费用"},
-					{Title: "诊断信息",},
-					{Title: "住址", Width: 150},
+					{Title: "#", Width: 22},
+					{Title: "姓名", Alignment: AlignNear, Width: 50},
+					{Title: "电话", Alignment: AlignNear, Width: 100},
+					{Title: "性别", Alignment: AlignNear, Width: 40},
+					{Title: "年龄", Alignment: AlignFar, Width: 40},
+					{Title: "诊费", Alignment: AlignFar, Width: 80},
+					{Title: "实收", Alignment: AlignFar, Width: 80},
+					{Title: "已付", Alignment: AlignFar, Width: 80},
+					{Title: "登记时间", Alignment: AlignCenter, Format: "2006-01-02", Width: 110},
+					{Title: "最新时间", Alignment: AlignCenter, Format: "2006-01-02", Width: 110},
+					{Title: "病理诊断", Alignment: AlignNear, Width: 130},
+					{Title: "治疗方案", Alignment: AlignNear, Width: 130},
+					{Title: "住址", Width: 130},
 				},
 				StyleCell: func(style *walk.CellStyle) {
-					item := model.items[style.Row()]
-
-					if item.checked {
-						if style.Row()%2 == 0 {
-							style.BackgroundColor = walk.RGB(159, 215, 255)
-						} else {
-							style.BackgroundColor = walk.RGB(143, 199, 239)
-						}
+					item := model.sItems[style.Row()]
+					if item.Checked {
+						style.BackgroundColor = walk.RGB(143, 199, 239)
 					}
-
 					switch style.Col() {
-					case 1:
-						if canvas := style.Canvas(); canvas != nil {
-							bounds := style.Bounds()
-							bounds.X += 2
-							bounds.Y += 2
-							bounds.Width = int((float64(bounds.Width) - 4) / 5 * float64(len(item.Bar)))
-							bounds.Height -= 4
-							canvas.DrawBitmapPartWithOpacity(barBitmap, bounds, walk.Rectangle{0, 0, 100 / 5 * len(item.Bar), 1}, 127)
-
-							bounds.X += 4
-							bounds.Y += 2
-							canvas.DrawText(item.Bar, tv.Font(), 0, bounds, walk.TextLeft)
-						}
-
-					case 2:
-						if item.Baz >= 900.0 {
-							style.TextColor = walk.RGB(0, 191, 0)
-							style.Image = goodIcon
-						} else if item.Baz < 100.0 {
+					case 7:
+						if item.PaidFee < item.RealFee {
 							style.TextColor = walk.RGB(255, 0, 0)
-							style.Image = badIcon
-						}
-
-					case 3:
-						if item.Quux.After(time.Now().Add(-365 * 24 * time.Hour)) {
-							style.Font = boldFont
 						}
 					}
 				},
 				Model: model,
-				OnSelectedIndexesChanged: func() {
-					fmt.Printf("SelectedIndexes: %v\n", tv.SelectedIndexes())
+				OnItemActivated: func() {
+					if cmd, err := AddDialog(mw, model.sItems[tv.SelectedIndexes()[0]]); err == nil && cmd == walk.DlgCmdOK {
+						model.ResetRows()
+					}
+				},
+			},
+			Composite{
+				Layout: Grid{Columns: 13},
+				Children: []Widget{
+					Label{
+						Text:    "合计收入:",
+						Font:    labelFont,
+						MaxSize: Size{Width: 50},
+						MinSize: Size{Width: 50},
+					},
+					Label{
+						Text:     strconv.FormatFloat(model.sum, 'f', 1, 64) + " 元",
+						AssignTo: &model.SumLabel,
+						Font:     labelFont,
+						MaxSize:  Size{Width: 100},
+						MinSize:  Size{Width: 100},
+					},
+					Label{
+						Text:    "当月总计:",
+						Font:    labelFont,
+						MaxSize: Size{Width: 50},
+						MinSize: Size{Width: 50},
+					},
+					Label{
+						Text:     strconv.FormatFloat(model.sSum, 'f', 1, 64) + " 元",
+						AssignTo: &model.LSumLabel,
+						Font:     labelFont,
+						MaxSize:  Size{Width: 100},
+						MinSize:  Size{Width: 100},
+					},
+					Label{
+						Text:    "差额总计:",
+						Font:    labelFont,
+						MaxSize: Size{Width: 50},
+						MinSize: Size{Width: 50},
+					},
+					Label{
+						Text:     strconv.FormatFloat(model.lSum, 'f', 1, 64) + " 元",
+						AssignTo: &model.SSumLabel,
+						Font:     labelFont,
+						MaxSize:  Size{Width: 100},
+						MinSize:  Size{Width: 100},
+					},
+					/*PushButton{
+						Text: "首页",
+						OnClicked: func() {
+
+						},
+					},
+					PushButton{
+						Text: "上一页",
+						OnClicked: func() {
+						},
+					},
+					LineEdit{
+						Text:    Bind("CurPage"),
+						MaxSize: Size{Width: inputWidth, Height: 20},
+						MinSize: Size{Width: inputWidth, Height: 20},
+					},
+					PushButton{
+						Text: "跳转",
+						OnClicked: func() {
+
+						},
+					},
+					PushButton{
+						Text: "下一页",
+						OnClicked: func() {
+
+						},
+					},
+					PushButton{
+						Text: "末页",
+						OnClicked: func() {
+
+						},
+					},*/
 				},
 			},
 		},
 	}.Run()
+}
+
+const (
+	SexWoman Sex = "女"
+	SexMan Sex = "男"
+)
+
+func AddDialog(owner walk.Form,foo *Foo) (int, error) {
+	var dlg *walk.Dialog
+	var db *walk.DataBinder
+	var acceptPB, cancelPB *walk.PushButton
+	addIcon, _ := walk.Resources.Icon("img/plus.png")
+	addFlag := foo==nil
+	if addFlag{
+		foo = new(Foo)
+		foo.Sex=SexMan
+	}
+	return Dialog{
+		AssignTo:      &dlg,
+		Icon: addIcon,
+		Background: SystemColorBrush{Color:walk.SysColorWindow},
+		Title:         Bind("添加病例"),
+		DefaultButton: &acceptPB,
+		CancelButton:  &cancelPB,
+		DataBinder: DataBinder{
+			AssignTo:       &db,
+			Name:           "foo",
+			DataSource:     foo,
+			ErrorPresenter: ToolTipErrorPresenter{},
+		},
+		MinSize: Size{Width:300},
+		MaxSize: Size{Width:400},
+		Layout:  VBox{},
+		Children: []Widget{
+			Composite{
+				Layout: Grid{Columns: 2},
+				Children: []Widget{
+					Label{
+						Text: "姓名:",
+					},
+					LineEdit{
+						Text: Bind("Name"),
+					},
+
+					Label{
+						Text: "联系电话:",
+					},
+					LineEdit{
+						Text: Bind("Phone"),
+					},
+
+					RadioButtonGroupBox{
+						ColumnSpan: 2,
+						Title:      "性别:",
+						Layout:     HBox{},
+						DataMember: "Sex",
+						Buttons: []RadioButton{
+							{Text: "男", Value: SexMan},
+							{Text: "女", Value: SexWoman},
+						},
+					},
+
+					Label{
+						Text: "年龄:",
+					},
+					NumberEdit{
+						Value:    Bind("Age"),
+						Decimals: 0,
+					},
+
+					Label{
+						Text: "病人住址:",
+					},
+					LineEdit{
+						Text: Bind("Address"),
+					},
+
+					Label{
+						Text: "病因诊断:",
+					},
+					TextEdit{
+						ColumnSpan: 2,
+						MinSize:    Size{Width: 100, Height: 80},
+						Text:       Bind("Diagnosed"),
+					},
+
+					Label{
+						Text: "治疗方案:",
+					},
+					TextEdit{
+						ColumnSpan: 2,
+						MinSize:    Size{Width: 100, Height: 80},
+						Text:       Bind("Program"),
+					},
+
+					Label{
+						Text: "就诊费用:",
+					},
+					NumberEdit{
+						Value:    Bind("AllFee"),
+						Suffix:   "$",
+						Decimals: 1,
+					},
+
+					Label{
+						Text: "实收费用:",
+					},
+					NumberEdit{
+						Value:    Bind("RealFee"),
+						Suffix:   "$",
+						Decimals: 1,
+					},
+
+					Label{
+						Text: "已付费用:",
+					},
+					NumberEdit{
+						Value:    Bind("PaidFee"),
+						Suffix:   "$",
+						Decimals: 1,
+					},
+				},
+			},
+			Composite{
+				Layout: HBox{},
+				Children: []Widget{
+					HSpacer{},
+					PushButton{
+						AssignTo: &acceptPB,
+						Text:     "保存",
+						OnClicked: func() {
+							if err := db.Submit(); err == nil {
+								foo.Update = time.Now()
+								if addFlag {
+									foo.Create = foo.Update
+									model.Head(foo)
+								}
+								Write(model.items)
+							}
+							dlg.Accept()
+						},
+					},
+					PushButton{
+						AssignTo:  &cancelPB,
+						Text:      "取消",
+						OnClicked: func() { dlg.Cancel() },
+					},
+				},
+			},
+		},
+	}.Run(owner)
 }
---
+++ src/data/data.go
@@ -0,0 +1,95 @@
+package data
+
+import (
+	"encoding/csv"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+)
+
+type Sex string
+
+type Foo struct {
+	Name		string
+	Phone		string
+	Create		time.Time
+	Update		time.Time
+	Diagnosed	string
+	Program		string
+	AllFee		float64
+	RealFee		float64
+	PaidFee		float64
+	Address		string
+	Age			int
+	Sex			Sex
+	Index		int
+	Checked 	bool
+}
+const data="data.csv"
+
+//Write 写入运行配置
+func Write(dabs []*Foo) {
+	//_ = os.Remove(data)
+	f, err := os.OpenFile(data, os.O_WRONLY|os.O_CREATE|os.O_TRUNC, os.ModePerm)
+
+	if err != nil {
+		fmt.Print("写文件失败！")
+	}
+	write:=csv.NewWriter(f)
+	records:=make([][]string, len(dabs)+1)
+	records[0]=[]string{"姓名","电话","登记时间","最新时间","病例诊断","治疗方案","就诊费用","实收费用","已付费用","住址","性别","年龄"}
+	for index,foo:=range dabs {
+
+		records[index+1]=[]string{foo.Name,foo.Phone,foo.Create.Format("2006-01-02 15:04:05"),foo.Update.Format("2006-01-02 15:04:05"),foo.Diagnosed,foo.Program,strconv.FormatFloat(foo.AllFee, 'f', 1, 64),strconv.FormatFloat(foo.RealFee, 'f', 1, 64),strconv.FormatFloat(foo.PaidFee, 'f', 1, 64),foo.Address,string(foo.Sex),strconv.Itoa(foo.Age)}
+	}
+	_ = write.WriteAll(records)
+
+	f.Close()
+	return
+}
+
+//Read 读取运行配置
+func Read() []*Foo {
+	file, err := os.Open(data)
+	if err != nil {
+		Write([]*Foo{}) //写入文件(字节数组)
+		file,_ = os.Open(data)
+	}
+	defer file.Close()
+
+	read := csv.NewReader(file)
+
+	records ,err:=read.ReadAll()
+	dabs := make([]*Foo, len(records)-1)
+	for index,_:= range dabs{
+		record:=records[index+1]
+		allFee,_ := strconv.ParseFloat(record[6],64)
+		realFee,_ := strconv.ParseFloat(record[7],64)
+		paidFee,_ := strconv.ParseFloat(record[8],64)
+		create, _ :=time.Parse("2006-01-02 15:04:05",record[2])
+		update, _ :=time.Parse("2006-01-02 15:04:05",record[3])
+		age, err := strconv.Atoi(record[11])
+		if err!=nil{
+			panic(err)
+		}
+		dabs[index] = &Foo{Name: record[0], Phone: record[1], Create: create, Update: update, Diagnosed: record[4], Program: record[5], AllFee: allFee, RealFee: realFee, PaidFee: paidFee, Address: record[9], Sex: Sex(record[10]), Age:age, Index: index}
+	}
+
+	file.Close()
+	return dabs
+}
+//
+//func main() {
+//	fobs :=Read()
+//	for index,_ := range fobs {
+//		foo:= fobs[index]
+//		fmt.Println(foo.Name,foo.Phone,foo.Create,foo.Update,foo.AllFee,foo.RealFee,foo.PaidFee,foo.Diagnosed,foo.Program,foo.Addr)
+//	}
+//	Write([]*Foo{{Name: "高阳", Phone: "18611754986",Create:"2018-01-01 12:00:00",Update:"2018-01-01 12:00:00",AllFee:"200",RealFee:"150",PaidFee:"50"}})
+//	fobs =Read()
+//	for index,_ := range fobs {
+//		foo:= fobs[index]
+//		fmt.Println(foo.Name,foo.Phone,foo.Create,foo.Update,foo.AllFee,foo.RealFee,foo.PaidFee,foo.Diagnosed,foo.Program,foo.Addr)
+//	}
+//}
